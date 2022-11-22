ee62382b33555f04e5c8e1af7326b367ab6a0446
ariel055132 <appleadrian100000@gmail.com>
2022-11-22 23:00:33 +0800

    update code and testcase of question 771

--- src/test/java/leetcode/editor/en/P771_JewelAndStone.java
+++ src/test/java/leetcode/editor/en/P771_JewelAndStone.java
@@ -19,4 +19,14 @@ public class P771_JewelAndStone {
         int actualResult = solution771.numJewelsInStones(jewels, stones);
         assertEquals(expectedResult, actualResult);
     }
+
+    @Test
+    void testExample2() {
+        setUp();
+        String jewels = "z";
+        String stones = "ZZ";
+        int expectedResult = 0;
+        int actualResult = solution771.numJewelsInStones(jewels, stones);
+        assertEquals(expectedResult, actualResult);
+    }
 }
