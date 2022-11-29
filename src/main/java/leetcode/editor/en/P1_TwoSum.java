c83128aaecac6677f21ec05d2c38edf87b526c45
ariel055132 <appleadrian100000@gmail.com>
2022-11-29 16:24:14 +0800

    update the solution and test case of question 1

--- src/main/java/leetcode/editor/en/P1_TwoSum.java
+++ src/main/java/leetcode/editor/en/P1_TwoSum.java
@@ -2,8 +2,14 @@ package leetcode.editor.en;
 
 // $P1_TwoSum
 
+import java.util.HashMap;
+
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution1 {
+
+    // Brute Force
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(1)
     public int[] twoSum(int[] nums, int target) {
         int[] result = new int[2];
         for (int i = 0; i < nums.length; i++) {
@@ -15,6 +21,28 @@ class Solution1 {
                 }
             }
         }
+        return result;
+    }
+
+    // Hash Table Approach
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    public int[] twoSum1(int[] nums, int target) {
+        int[] result = new int[2];
+        HashMap<Integer, Integer> hashMap = new HashMap<Integer, Integer>(nums.length);
+        for (int index = 0; index < nums.length; index++) {
+            hashMap.put(nums[index], index);
+        }
+        for (int index = 0; index < nums.length; index++) {
+            if (hashMap.get(target - nums[index]) != null && hashMap.get(target - nums[index]) != index) {
+                result[0] = index;
+                result[1] = hashMap.get(target - nums[index]);
+                break;
+            } else {
+                continue;
+            }
+        }
+
         return result;
     }
 }
--- src/test/java/leetcode/editor/en/P1_TwoSum.java
+++ src/test/java/leetcode/editor/en/P1_TwoSum.java
@@ -14,12 +14,62 @@ public class P1_TwoSum {
     }
 
     @Test
-    void testTwoSum1() {
+    void testExample1_method1() {
         setUp();
         int[] nums = {2, 7, 11, 15};
         int target = 9;
         int[] expectedResult = {0, 1};
         int[] actualResult = solution1.twoSum(nums, target);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testExample2_method1() {
+        setUp();
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] expectedResult = {1, 2};
+        int[] actualResult = solution1.twoSum(nums, target);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testExample3_method1() {
+        setUp();
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] expectedResult = {0, 1};
+        int[] actualResult = solution1.twoSum(nums, target);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testExample1_method2() {
+        setUp();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] expectedResult = {0, 1};
+        int[] actualResult = solution1.twoSum1(nums, target);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testExample2_method2() {
+        setUp();
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        int[] expectedResult = {1, 2};
+        int[] actualResult = solution1.twoSum1(nums, target);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testExample3_method3() {
+        setUp();
+        int[] nums = {3, 3};
+        int target = 6;
+        int[] expectedResult = {0, 1};
+        int[] actualResult = solution1.twoSum1(nums, target);
         assertArrayEquals(expectedResult, actualResult);
     }
 }
