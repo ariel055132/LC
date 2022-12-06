9377ddb39b5238f77bb0eabf512ae639fe86f167
ariel055132 <appleadrian100000@gmail.com>
2022-12-07 07:48:50 +0800

    update solution of question 876

---
+++ src/main/java/leetcode/editor/en/P876_MiddleOfTheLinkedList.java
@@ -0,0 +1,27 @@
+package leetcode.editor.en;
+
+// $P876_MiddleOfTheLinkedList
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution876 {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
