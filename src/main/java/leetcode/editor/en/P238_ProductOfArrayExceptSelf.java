b9d3742513823b54c7763d1eadc84b1d65c88b73
ariel055132 <appleadrian100000@gmail.com>
2022-12-16 11:18:21 +0800

    update of question 238

--- src/main/java/leetcode/editor/en/P238_ProductOfArrayExceptSelf.java
+++ src/main/java/leetcode/editor/en/P238_ProductOfArrayExceptSelf.java
@@ -25,5 +25,37 @@ class Solution238 {
         }
         return answer;
     }
+
+    // Approach 2: Brute Force
+    // Time Complexity: O(n^2)
+    // Space Complexity: O(n)
+    public int[] productExceptSelf1(int[] nums) {
+        int[] result = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            int tmp = 1;
+            for (int j = 0; j < nums.length; j++) {
+                if (i == j) continue;
+                else tmp *= nums[j];
+            }
+            result[i] = tmp;
+        }
+        return result;
+    }
+
+    // Approach 3: Divide the product of array with the number
+    // Time Complexity: O(n)
+    // Space Complexity: O(n)
+    // Cons: Cannot handle element 0 in the array. (Divide 0 @.@)
+    public int[] productExceptSelf2(int[] nums) {
+        int[] result = new int[nums.length];
+        int tmp = 1;
+        for (int num : nums) {
+            tmp *= num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            result[i] = tmp / nums[i];
+        }
+        return result;
+    }
 }
 //leetcode submit region end(Prohibit modification and deletion)
--- src/test/java/leetcode/editor/en/P238_ProductOfArrayExceptSelf.java
+++ src/test/java/leetcode/editor/en/P238_ProductOfArrayExceptSelf.java
@@ -12,7 +12,7 @@ public class P238_ProductOfArrayExceptSelf {
     }
 
     @Test
-    void testExample1() {
+    void testExample1_approach1() {
         setUp();
         int[] nums = {1, 2, 3, 4};
         int[] expectedResult = {24, 12, 8, 6};
@@ -21,11 +21,29 @@ public class P238_ProductOfArrayExceptSelf {
     }
 
     @Test
-    void testExample2() {
+    void testExample2_approach1() {
         setUp();
         int[] nums = {-1, 1, 0, -3, 3};
         int[] expectedResult = {0, 0, 9, 0, 0};
         int[] actualResult = solution238.productExceptSelf(nums);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testExample1_approach2() {
+        setUp();
+        int[] nums = {1, 2, 3, 4};
+        int[] expectedResult = {24, 12, 8, 6};
+        int[] actualResult = solution238.productExceptSelf1(nums);
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    void testExample2_approach2() {
+        setUp();
+        int[] nums = {-1, 1, 0, -3, 3};
+        int[] expectedResult = {0, 0, 9, 0, 0};
+        int[] actualResult = solution238.productExceptSelf1(nums);
         assertArrayEquals(expectedResult, actualResult);
     }
 }
