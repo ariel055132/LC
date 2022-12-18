1504e04d3fc56f7f1dd0cacd72c500edd5eede53
ariel055132 <appleadrian100000@gmail.com>
2022-12-18 20:55:02 +0800

    update solution and testcase of question 75

---
+++ src/main/java/leetcode/editor/en/P75_SortColors.java
@@ -0,0 +1,33 @@
+package leetcode.editor.en;
+
+// $P75_SortColors
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public void sortColors(int[] nums) {
+        int zeroCount = 0;
+        int oneCount = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                zeroCount++;
+            } if (num == 1) {
+                 oneCount++;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (zeroCount > 0) {
+                nums[i] = 0;
+                zeroCount--;
+            } else if (oneCount > 0) {
+                nums[i] = 1;
+                oneCount--;
+            } else {
+                nums[i] = 2;
+            }
+        }
+    }
+
+
+}
+//leetcode submit region end(Prohibit modification and deletion)
