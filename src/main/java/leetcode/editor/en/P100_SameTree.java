06e6fc7764b27cfda15a0c0e6321239b0c35cc16
Adrian Li <36882230+ariel055132@users.noreply.github.com>
2023-01-11 10:47:02 +0800

    upload the testcase and solution of question 100

---
+++ src/main/java/leetcode/editor/en/P100_SameTree.java
@@ -0,0 +1,37 @@
+package leetcode.editor.en;
+
+// $P100_SameTree
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution100 {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        // both are empty tree --> return true
+        if (p == null && q == null) {
+            return true;
+        }
+        // one of them is empty --> return false
+        else if ( p == null || q == null ) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
